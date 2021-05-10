70282111b11d842fd63c5bc406f13b79e159d52f
adrian <elaawers@gmail.com>
2021-05-10 10:42:54 +0700

    [law] adjust highest integer question

--- April/[level 1]Highest_Integer/Highest_Integer.js
+++ April/[level 1]Highest_Integer/Highest_Integer.js
@@ -7,7 +7,7 @@ const test = (testCase, result) => {
     return console.log(true);
 };
 
-test(find("10 9 2 5 1 2 4 0 56"),'56 10 4 9 2 4 5 1 ^ ')
+test(find("10 9 2 5 1 2 4 0 56"),'56 10 4 9 2 4 5 1 ^')
 test(find("3 8 99 3 2 44 12"),'8 12 ^ 8 3 99 44')
 
 
